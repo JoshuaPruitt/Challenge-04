d7d00a2f22ff68af1fe2b5a32293d3ec653997e9
JoshuaPruitt <JoshuaPruitt6484@gmail.com>
2024-06-30 18:12:44 -0600

    added code for the form site before blog page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,9 +1,22 @@
 // TODO: Create a variable that selects the main element
-
+const mainEl = document.getElementsByTagName('main')
 // TODO: Create a function that builds an element and appends it to the DOM
 
+function pageBuild (){
+
+}
 // TODO: Create a function that handles the case where there are no blog posts to display
 
+function noBlog (){
+
+}
 // TODO: Create a function that reads from local storage and returns the data
 
+function pageStorage (){
+
+}
 // TODO: Call the function to render the list of blog posts
+
+function renderPosts(){
+
+}
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,32 @@
 // TODO: Create a variable that selects the form element
+const formEl = document.getElementsByTagName('form')
+const error = document.getElementById('error')
+const usernameId = document.getElementById('username')
+const titleId = document.getElementById('title')
+const bodyId = document.getElementById('bodyContent')
+const submitId = document.getElementById('submit')
 
-// TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
-
+// set the url to blank. Define the storage
 let redirectURL = '';
+let infoStorage;
+
+// initilize at page startup. Keeps infostorage from being overwritten to be blank
+if (infoStorage == null){
+  infoStorage = [];
+
+} else {
+  console.log("working!")
+  infoStorage = JSON.parse(localStorage.getItem('pageInformation'));
+  console.log(infoStorage);
+}
+
+function formSubmission(){
+  let tempPageStorage = {username: usernameId.value, title: titleId.value, body: bodyId.value}
+  infoStorage.push(tempPageStorage)
+
+  // sends the collected information to local storage
+  localStorage.setItem('pageInformation', JSON.stringify(infoStorage));
+}
 
 const redirectPage = function (url) {
   redirectURL = url;
@@ -10,3 +34,18 @@ const redirectPage = function (url) {
 };
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
+submitId.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  if (usernameId.value == null || titleId.value == null || bodyId.value == null){
+    error.textContent = 'Error! Username, title, or body is missing! Please add then submit';
+    error.append;
+
+  } else {
+    //push info to local storage
+    formSubmission()
+
+    //redirect page
+    redirectPage('blog.html')
+  }
+});
